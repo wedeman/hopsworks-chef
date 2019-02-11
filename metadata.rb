0962bf838b291f8f27a49975ce090e048a19689f
Jim Dowling <jdowling@sics.se>
2019-02-11 17:13:54 +0100

    add cvat to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -35,6 +35,7 @@ depends 'hopslog'
 depends 'hopsmonitor'
 depends 'hive2'
 depends 'hops_airflow'
+depends 'cvat'
 
 #link:Click <a target='_blank' href='https://%host%:4848'>here</a> to launch Glassfish in your browser (http)
 recipe  "hopsworks::install", "Installs Glassfish"
